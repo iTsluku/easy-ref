59713b11181773835abbc9e4dc83b5853cc534da
einwille <einwille@fim.uni-passau.de>
2022-02-19 03:34:58 +0100

    Add link-container mock, Add custimization page (for sortable drag and drop), Add container structure

--- App.py
+++ App.py
@@ -3,22 +3,57 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 containers = [
-    {"title": "Ref Stack A", "description": "bla bla bla"},
-    {"title": "Ref Stack B", "description": "bla bla bla bla bla bla"},
-    {"title": "Ref Stack C", "description": "bla bla bla bla bla"},
-    {"title": "Ref Stack D", "description": "bla bla bla"},
-    {"title": "Ref Stack E", "description": "bla bla bla"},
-    {"title": "Ref Stack F", "description": "bla bla bla"},
-    {"title": "Ref Stack G", "description": "bla bla bla"},
-    {"title": "Ref Stack H", "description": "bla bla bla bla bla bla bla"},
     {
-        "title": "Ref Stack I",
-        "description": "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+        "title": "Media",
+        "links": [
+            {"title": "YouTube", "link": "https://www.youtube.com/"},
+            {"title": "Twitch", "link": "https://www.twitch.tv/"},
+            {"title": "Twitter", "link": "https://twitter.com/home"},
+        ],
     },
-    {"title": "Ref Stack J", "description": "bla bla bla"},
-    {"title": "Ref Stack K", "description": "bla bla bla"},
-    {"title": "Ref Stack L", "description": "bla bla bla bla bla"},
-    {"title": "Ref Stack M", "description": "bla bla bla"},
+    {
+        "title": "E-Mail",
+        "links": [
+            {"title": "GMail", "link": "https://mail.google.com/"},
+            {
+                "title": "Outlook",
+                "link": "https://email.uni-passau.de/",
+            },
+        ],
+    },
+    {
+        "title": "Thesis",
+        "links": [
+            {"title": "NDB", "link": "https://www.deutsche-biographie.de/"},
+            {"title": "NDB API", "link": "http://data.deutsche-biographie.de/about/"},
+            {
+                "title": "Wikidata",
+                "link": "https://www.wikidata.org/wiki/Wikidata:Main_Page",
+            },
+        ],
+    },
+    {
+        "title": "Python",
+        "links": [
+            {"title": "Documentation", "link": "https://docs.python.org/3/"},
+            {"title": "Regex", "link": "https://docs.python.org/3/library/re.html"},
+            {
+                "title": "Mock",
+                "link": "https://docs.python.org/3/library/unittest.mock.html",
+            },
+        ],
+    },
+    {
+        "title": "Temp link dump",
+        "links": [
+            {"title": "R", "link": "https://packages.othr.de/cran/"},
+            {"title": "i3wm", "link": "https://i3wm.org/"},
+            {
+                "title": "read blog post",
+                "link": "https://www.kalzumeus.com/2011/10/28/dont-call-yourself-a-programmer/",
+            },
+        ],
+    },
 ]
 
 
@@ -36,6 +71,11 @@ def page_not_found(e):
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/customization")
+def customization():
+    return render_template("customization.html", containers=containers)
 
 
 if __name__ == "__main__":
