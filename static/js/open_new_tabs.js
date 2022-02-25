db8342b1ee9de4245d67e0c53a5ca6b0fd25902d
einwille <einwille@fim.uni-passau.de>
2022-02-25 04:15:11 +0100

    Add opening links of containers functionality

---
+++ static/js/open_new_tabs.js
@@ -0,0 +1,18 @@
+var linkContainer = document.getElementsByClassName('open-new-tabs');
+
+for (var i=0, c; c = linkContainer[i]; i++) {
+    var collection = c.getElementsByClassName('link-container-item-link');
+    links = [];
+    for (let c of collection) {
+        links.push(c.innerText);
+    }
+    c.addEventListener('click',openNewTabs,false);
+    c.links=links;
+}
+
+function openNewTabs(event){
+    var links = event.currentTarget.links;
+    for (let i = 0; i < links.length; i++) {
+        window.open(links[i]);
+    }
+};
